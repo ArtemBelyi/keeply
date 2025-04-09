e5c3665f896d1acf4e1e7c47a633f70c93bcd2a4
belyj_ai <belyj_ai@teleformis.ru>
2025-04-09 22:19:53 +0300

    modified menu-items component

--- src/app/commons/components/menu-items/menu-items.component.ts
+++ src/app/commons/components/menu-items/menu-items.component.ts
@@ -1,18 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, TemplateRef } from '@angular/core';
 import { MenuItem } from "../../../models/menu-item.model";
-import { MatMenu, MatMenuItem, MatMenuTrigger } from "@angular/material/menu";
 import { NgForOf, NgIf, NgTemplateOutlet } from "@angular/common";
 import { MatButton } from "@angular/material/button";
-import { MenuItemComponent } from "../menu-item/menu-item.component";
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from "@angular/cdk/menu";
 
 @Component({
   selector: 'app-menu-items',
   standalone: true,
-  imports: [MatMenu, MatMenuTrigger, NgForOf, MatMenuItem, MatButton, NgIf, NgTemplateOutlet, MenuItemComponent],
+  imports: [ NgForOf, MatButton, NgIf, NgTemplateOutlet, CdkMenuItem, CdkMenu, CdkMenuTrigger],
   templateUrl: './menu-items.component.html',
   styleUrl: './menu-items.component.css'
 })
 export class MenuItemsComponent {
   @Input() menuItems: MenuItem[] = [];
-  @Input() buttonLabel = 'Menu';
+  @ViewChild("menu", { static: true }) menu!: TemplateRef<MenuItemsComponent>;
 }
--- src/app/components/toolbar/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar/toolbar.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenuItemsComponent } from "../../../commons/components/menu-items/menu-items.component";
 import { ToolbarService } from "../../../services/toolbar.service";
 import { MenuItem } from "../../../models/menu-item.model";
+import { MatButton } from "@angular/material/button";
+import { CdkMenuTrigger } from "@angular/cdk/menu";
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [ MenuItemsComponent ],
+  imports: [MenuItemsComponent, MatButton, CdkMenuTrigger],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
-export class ToolbarComponent implements OnInit {
-
+export class ToolbarComponent {
   fileBtnLabel: string = "File";
   fileBtnItems: MenuItem[] = [];
 
-  constructor(private toolbarService: ToolbarService) {}
-
-  ngOnInit() {
+  constructor(private toolbarService: ToolbarService) {
     this.fileBtnItems = this.toolbarService.fileItemsBtn;
   }
 }
