de4acb5fec45b6bfac51960667007d3abe15e206
belyj_ai <belyj_ai@teleformis.ru>
2025-05-12 23:01:10 +0300

    removed component store and installed signal store

--- source/renderer/app/features/vault/store/vault.store.ts
+++ source/renderer/app/features/vault/store/vault.store.ts
@@ -1,16 +1,1 @@
-import { Injectable } from '@angular/core';
-import { VaultState } from "../models/vault.model";
-import { ComponentStore } from "@ngrx/component-store";
 
-
-@Injectable()
-export class VaultStore extends ComponentStore<VaultState> {
-
-  constructor() {
-    super({
-      name: 'Vault',
-      entries: [],
-      groups: []
-    })
-  }
-}
