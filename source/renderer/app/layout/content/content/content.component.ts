30494f065f13f716a9285ed78de15eb8475d8a7f
belyj_ai <belyj_ai@teleformis.ru>
2025-05-19 01:09:45 +0300

    created content component and tabs

--- source/renderer/app/layout/content/content/content.component.ts
+++ source/renderer/app/layout/content/content/content.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { VaultComponent } from "../../../features/vault/vault.component";
+import { MatTab, MatTabGroup, MatTabLabel } from "@angular/material/tabs";
+import { MatIconModule } from "@angular/material/icon";
+
+type Tab = {
+  id: number;
+  name: string;
+}
 
 @Component({
-    selector: 'app-content',
-    imports: [VaultComponent],
-    templateUrl: './content.component.html',
-    standalone: true,
-    styleUrl: './content.component.css'
+  selector: 'app-content',
+  imports: [VaultComponent, MatTabGroup, MatTab, MatIconModule, MatTabLabel],
+  templateUrl: './content.component.html',
+  standalone: true,
+  styleUrl: './content.component.css'
 })
-export class ContentComponent {}
+export class ContentComponent {
+  selectedId = signal<number>(0);
+  tabs: Tab[] = [
+    { id: 1, name: "Vault_1" },
+    { id: 2, name: "Vault_2" },
+    { id: 3, name: "Vault_3" }
+  ]
+
+  addTab(event: MouseEvent) {
+    event.stopPropagation();
+
+  }
+
+  setSelectedId(id: number) {
+    this.selectedId.set(id);
+  };
+}
