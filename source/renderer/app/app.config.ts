a76e09774ae058a4275c1f74915c9b22f8e62e8d
belyj_ai <belyj_ai@teleformis.ru>
2025-07-07 20:55:48 +0300

    edited style scc

--- source/renderer/app/app.config.ts
+++ source/renderer/app/app.config.ts
@@ -1,7 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import { definePreset } from "@primeng/themes";
 import Aura from '@primeng/themes/aura';
+
+const KeeplyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}'
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -9,7 +28,7 @@ export const appConfig: ApplicationConfig = {
      provideAnimationsAsync(),
         providePrimeNG({
             theme: {
-                preset: Aura
+                preset: KeeplyPreset
             }
         })
   ]
